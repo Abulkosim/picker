e5db9f424da59109fc7c23809f057470b4321d6d
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-10-08 16:19:30 +0500

    timepicker done

--- src/components/TimePicker.vue
+++ src/components/TimePicker.vue
@@ -1,85 +1,89 @@
 <template>
-  <div class="time-picker">
+  <div class="time-picker" @click="open = true" @click.stop>
     <!-- <input class="text-input" type="text" v-model="str" @input="event => parseValue(event.target.value)"> -->
     <div class="display">
       <input type="number" v-model="time.hours" min="0" max="23" v-if="props.format != '12'" @input="resize">
       <input type="number" v-model="time.hours" min="0" max="11" v-if="props.format == '12'" @input="resize">
+      <span>:</span>
       <input type="number" v-model="time.minutes" min="0" max="59" @input="resize">
+      <span v-if="props.seconds">:</span>
       <input type="number" v-model="time.seconds" min="0" max="59" @input="resize" v-if="props.seconds">
       <button @click="isAm = !isAm" v-if="props.format == '12'">{{ ampm }}</button>
     </div>
 
-    <div class="content-container">
-      <div class="content">
-        <div class="item" v-if="props.format != '12'">
-          <div class="icon-div">
-            <PhCaretUp :size="20" @click="changeHours(1)" />
+    <Transition>
+      <div class="content-container" v-if="open" @click.stop>
+        <div class="content">
+          <div class="item" v-if="props.format != '12'">
+            <div class="icon-div">
+              <PhCaretUp :size="20" @click="changeHours(1)" />
+            </div>
+            <input type="number" v-model="time.hours" min="0" max="23">
+            <div class="icon-div">
+              <PhCaretDown :size="20" @click="changeHours(-1)" />
+            </div>
           </div>
-          <input type="number" v-model="time.hours" min="0" max="23">
-          <div class="icon-div">
-            <PhCaretDown :size="20" @click="changeHours(-1)" />
-          </div>
-        </div>
 
-        <div class="item" v-if="props.format == '12'">
-          <div class="icon-div">
-            <PhCaretUp :size="20" @click="changeHours12(1)" />
+          <div class="item" v-if="props.format == '12'">
+            <div class="icon-div">
+              <PhCaretUp :size="20" @click="changeHours12(1)" />
+            </div>
+            <input type="number" v-model="time.hours" min="0" max="11">
+            <div class="icon-div">
+              <PhCaretDown :size="20" @click="changeHours12(-1)" />
+            </div>
           </div>
-          <input type="number" v-model="time.hours" min="0" max="11">
-          <div class="icon-div">
-            <PhCaretDown :size="20" @click="changeHours12(-1)" />
-          </div>
-        </div>
 
-        <div class="item">
-          <span>:</span>
-        </div>
-
-        <div class="item">
-          <div class="icon-div">
-            <PhCaretUp :size="20" @click="changeMinutes(1)" />
+          <div class="item">
+            <span>:</span>
           </div>
-          <input type="number" v-model="time.minutes" min="0" max="59">
-          <div class="icon-div">
-            <PhCaretDown :size="20" @click="changeMinutes(-1)" />
-          </div>
-        </div>
 
-        <div class="item" v-if="props.seconds">
-          <span>:</span>
-        </div>
-
-        <div class="item" v-if="props.seconds">
-          <div class="icon-div">
-            <PhCaretUp :size="20" @click="changeSeconds(1)" />
+          <div class="item">
+            <div class="icon-div">
+              <PhCaretUp :size="20" @click="changeMinutes(1)" />
+            </div>
+            <input type="number" v-model="time.minutes" min="0" max="59">
+            <div class="icon-div">
+              <PhCaretDown :size="20" @click="changeMinutes(-1)" />
+            </div>
           </div>
-          <input type="number" v-model="time.seconds" min="0" max="59">
-          <div class="icon-div">
-            <PhCaretDown :size="20" @click="changeSeconds(-1)" />
-          </div>
-        </div>
 
-        <div class="item" v-if="props.format == '12'">
-          <span>:</span>
-        </div>
-
-        <div class="item" v-if="props.format == '12'">
-          <div class="icon-div">
-            <PhCaretUp :size="20" @click="isAm = !isAm" />
+          <div class="item" v-if="props.seconds">
+            <span>:</span>
           </div>
-          <button @click="isAm = !isAm">{{ ampm }}</button>
-          <div class="icon-div">
-            <PhCaretDown :size="20" @click="isAm = !isAm" />
+
+          <div class="item" v-if="props.seconds">
+            <div class="icon-div">
+              <PhCaretUp :size="20" @click="changeSeconds(1)" />
+            </div>
+            <input type="number" v-model="time.seconds" min="0" max="59">
+            <div class="icon-div">
+              <PhCaretDown :size="20" @click="changeSeconds(-1)" />
+            </div>
+          </div>
+
+          <div class="item" v-if="props.format == '12'">
+            <span>:</span>
+          </div>
+
+          <div class="item" v-if="props.format == '12'">
+            <div class="icon-div">
+              <PhCaretUp :size="20" @click="isAm = !isAm" />
+            </div>
+            <button @click="isAm = !isAm">{{ ampm }}</button>
+            <div class="icon-div">
+              <PhCaretDown :size="20" @click="isAm = !isAm" />
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Transition>
   </div>
 </template>
 
 <script setup>
 import { PhCaretUp, PhCaretDown } from "@phosphor-icons/vue";
-import { ref, computed, onMounted, reactive } from 'vue';
+import { ref, computed } from 'vue';
 
 const props = defineProps({
   format: String,
@@ -96,7 +100,7 @@ function changeHours(val) {
   if (+time.value.hours == 0 && val < 0) {
     return " "
   } else {
-    time.value.hours = (+time.value.hours + val) % 24
+    time.value.hours = ("0" + ((+time.value.hours + val) % 24)).slice(-2)
   }
 }
 
@@ -104,7 +108,7 @@ function changeHours12(val) {
   if (+time.value.hours == 0 && val < 0) {
     return " "
   } else {
-    time.value.hours = (+time.value.hours + val) % 12
+    time.value.hours = ("0" + ((+time.value.hours + val) % 12)).slice(-2)
   }
 }
 
@@ -112,7 +116,7 @@ function changeMinutes(val) {
   if (+time.value.minutes == 0 && val < 0) {
     return " "
   } else {
-    time.value.minutes = (+time.value.minutes + val) % 60
+    time.value.minutes = ("0" + ((+time.value.minutes + val) % 60)).slice(-2)
   }
 }
 
@@ -120,7 +124,7 @@ function changeSeconds(val) {
   if (+time.value.seconds == 0 && val < 0) {
     return " "
   } else {
-    time.value.seconds = (+time.value.seconds + val) % 60
+    time.value.seconds = ("0" + ((+time.value.seconds + val) % 60)).slice(-2)
   }
 }
 
@@ -161,12 +165,20 @@ let ampm = computed(() => {
 // }
 
 
-function resize(event) {
-  event.target.style.width = event.target.value.length + "ch"
-  if (event.target.value.length > 0) {
-    event.target.style.margin = '0 2px'
-  }
-}
+// function resize(event) {
+//   if (event.target.value.length == 0) {
+//     event.target.style.width = '1ch'
+//   } else {
+//     // event.target.style.width = event.target.value.length + "ch"
+//     console.log(event.target.style.width)
+//   }
+// }
+
+const open = ref(false)
+
+window.addEventListener('click', () => {
+  open.value = !open.value
+})
 </script>
 
 <style scoped>
@@ -179,11 +191,11 @@ function resize(event) {
 }
 
 .display {
-  @apply flex border-2 w-full p-2 rounded-md text-lg font-medium text-[#666] select-none
+  @apply flex gap-0.5 border-2 w-full p-2 rounded-md text-lg font-medium text-[#666] select-none cursor-pointer
 }
 
 .display input {
-  @apply border-none outline-none w-6;
+  @apply border-none outline-none w-6 text-center;
 }
 
 .content-container {
@@ -207,7 +219,7 @@ function resize(event) {
 }
 
 span {
-  @apply text-xl
+  @apply text-xl mt-[-1.5px]
 }
 
 input::-webkit-outer-spin-button,
@@ -222,5 +234,15 @@ input[type=number] {
 
 button {
   @apply outline-none
+}
+
+.v-enter-active,
+.v-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.v-enter-from,
+.v-leave-to {
+  opacity: 0;
 }
 </style>
