eba84d77ef78239459574be65d9423063217277c
Abulkosim Kataev <abulkasimkattaev@gmail.com>
2023-10-15 11:42:45 +0500

    fix: fixed mistakes

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="m-5">
     <TimePicker format="12" seconds min="3722" max="36122" @result="modelData($event)" />
-    <h1 class="text-xl my-4 text-[#666] font-semibold">{{ time }}</h1>
+    <h1 class="value">{{ time }}</h1>
   </div>
 </template>
 
@@ -11,8 +11,7 @@ import TimePicker from "./components/TimePicker.vue"
 
 const time = ref('')
 
-function modelData(e) {
-  time.value = e;
+function modelData(event) {
+  time.value = event;
 }
-
 </script>
--- src/components/TimePicker.vue
+++ src/components/TimePicker.vue
@@ -1,12 +1,12 @@
 <template>
   <div class="time-picker" @click="open = true; model()" @click.stop>
     <div class="display">
-      <input type="number" v-model="time.hours" min="0" max="23" v-if="props.format != '12'" @input="resize">
-      <input type="number" v-model="time.hours" min="0" max="11" v-if="props.format == '12'" @input="resize">
+      <input type="number" v-model="time.hours" min="0" max="23" v-if="props.format != '12'">
+      <input type="number" v-model="time.hours" min="0" max="11" v-if="props.format == '12'">
       <span>:</span>
-      <input type="number" v-model="time.minutes" min="0" max="59" @input="resize">
+      <input type="number" v-model="time.minutes" min="0" max="59">
       <span v-if="props.seconds">:</span>
-      <input type="number" v-model="time.seconds" min="0" max="59" @input="resize" v-if="props.seconds">
+      <input type="number" v-model="time.seconds" min="0" max="59" v-if="props.seconds">
       <button @click="isAm = !isAm" v-if="props.format == '12'">{{ ampm }}</button>
     </div>
 
@@ -74,10 +74,10 @@
               <PhCaretDown :size="18" @click="isAm = !isAm" />
             </div>
           </div>
+
         </div>
       </div>
     </Transition>
-
   </div>
 </template>
 
@@ -105,6 +105,7 @@ const props = defineProps({
 })
 
 const open = ref(false)
+
 const time = ref({
   hours: ("0" + 0).slice(-2),
   minutes: ("0" + 0).slice(-2),
@@ -212,68 +213,3 @@ const obj = computed(() => {
 })
 
 </script>
-<style scoped>
-.time-picker {
-  @apply w-96;
-}
-
-.text-input {
-  @apply border-2 w-full p-2 rounded-md outline-none caret-[#666] focus:border-sky-500 focus:outline-sky-200 focus:outline-2 focus:outline-offset-0 text-lg font-medium text-[#666]
-}
-
-.display {
-  @apply flex gap-0.5 border-2 w-full p-2 rounded-md text-lg font-medium text-[#666] select-none cursor-pointer
-}
-
-.display input {
-  @apply border-none outline-none w-6 text-center;
-}
-
-.content-container {
-  @apply w-full h-28 mt-1 border shadow-sm rounded-md flex items-center justify-center
-}
-
-.content {
-  @apply flex gap-2 items-center select-none
-}
-
-.item {
-  @apply flex flex-col items-center font-semibold text-[#666] w-6 justify-center
-}
-
-.item input {
-  @apply border-none outline-none w-full text-center
-}
-
-.icon-div {
-  @apply w-5 h-5 rounded-full flex items-center justify-center hover:bg-[#f3f3f3] cursor-pointer active:bg-[#eeeeee]
-}
-
-span {
-  @apply text-xl mt-[-1.5px]
-}
-
-input::-webkit-outer-spin-button,
-input::-webkit-inner-spin-button {
-  -webkit-appearance: none;
-  margin: 0;
-}
-
-input[type=number] {
-  -moz-appearance: textfield;
-}
-
-button {
-  @apply outline-none
-}
-
-.v-enter-active,
-.v-leave-active {
-  transition: opacity 0.5s ease;
-}
-
-.v-enter-from,
-.v-leave-to {
-  opacity: 0;
-}
-</style>
